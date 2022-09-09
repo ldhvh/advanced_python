f175c9ee65234d8391f651da23df4ecbc0f0f696
ldhvh <96458106+ldhvh@users.noreply.github.com>
2022-09-09 17:13:50 +0600

    first day first lesson

---
+++ independent work - practice2/third11.py
@@ -0,0 +1,4 @@
+import math
+x = 2
+y = 1
+print(x**x*y+math.pow(x,math.pow(x,y))-x**4)
---
+++ independent work - practice2/third12.py
@@ -0,0 +1,5 @@
+import math
+x =1
+y =4
+z=3
+print((math.fabs(math.pow((1/math.tan(y))+6)),(1./3.))+math.sqrt(((x+1)**3)/(4*y-2*z)))
