7dcc33fd566f31d3fe7a6fe843ccffd0e33ac542
ldhvh <96458106+ldhvh@users.noreply.github.com>
2022-09-19 18:46:05 +0600

    update

--- week2/indpndt.py
+++ week2/indpndt.py
@@ -5,5 +5,19 @@ print("The reverse list is ","".join(li))
 
 #task2
 print()
-def change(list
+def change(list):
+    list[0], list[len(list)-1] = list[len(list)-1], list[0]
+    return list
+print("List with swapped first and last element: ","".join(change(li)))
+
+#task3
+print()
+def to_list(*args):
+  print([i for i in args])
+print("The lists are:")
+to_list(10,20,30)
+to_list(4,2)
+to_list(4,8,3,5,1,9,3)
     
+#task4
+print()
