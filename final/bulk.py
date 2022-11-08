6ed8ce622bcfa1442255116e11516d31eae716ed
ldhvh <96458106+ldhvh@users.noreply.github.com>
2022-11-08 16:05:20 +0600

    updated

--- final/bulk.py
+++ final/bulk.py
@@ -1,18 +1,13 @@
 import os
 from PIL import Image
 
+image_dir = os.listdir('testdir')
 
-img_dir = "testdir/"
-dirs = os.listdir( img_dir )
-
-def resize_bulk_images():
-	for img in dirs:
-		if os.path.isfile(img_dir + img):
-			im = Image.open(img_dir + img)
-			f, e = os.path.splitext(img_dir + img)
-			imResize = im.resize((1200, 628), Image.ANTIALIAS)
-			imResize.save(f + '_resized.jpg', 'JPEG', quality=90)
-
-print('Bulk images resizing started...')
-resize_bulk_images()
-print('Bulk images resizing finished...')
+for image in image_dir:
+    im = Image.open('testdir/' + image)
+    width, height = im.size
+    new_width = 1000
+    ratio = new_width/width
+    new_height = int(height * ratio)
+    new_image = im.resize((new_width, new_height))
+    new_image.save('testdir/' + image, optimize = True, quality = 30)
